b1875d069ab24db78650723ebf08c22e7a5a499b
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-08-08 18:11:36 +0300

    tests_12_3.py

--- tests_12_3.py
+++ tests_12_3.py
@@ -1,23 +1,37 @@
 import unittest
 from runner import Runner
-from runner_tournament import Tournament
+from runner_tournament import Runner, Tournament
+from functools import wraps
+
+
+def skip_if_frozen(test_case):
+    @wraps(test_case)
+    def wrapper(self, *args, **kwargs):
+        if getattr(self, 'is_frozen', False):
+            print("Тесты в этом кейсе заморожены")
+            raise unittest.SkipTest("Тесты в этом кейсе заморожены")
+        return test_case(self, *args, **kwargs)
+    return wrapper
 
 
 class RunnerTest(unittest.TestCase):
-    is_frozen = False
+    is_frozen = False  # Атрибут для RunnerTest
 
+    @skip_if_frozen
     def test_walk(self):
         runner = Runner("бегун")
         for _ in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
+    @skip_if_frozen
     def test_run(self):
         runner = Runner("бегун")
         for _ in range(10):
             runner.run()
         self.assertEqual(runner.distance, 100)
 
+    @skip_if_frozen
     def test_challenge(self):
         runner1 = Runner("бегун 1")
         runner2 = Runner("бегун 2")
@@ -26,17 +40,9 @@ class RunnerTest(unittest.TestCase):
             runner2.walk()
         self.assertNotEqual(runner1.distance, runner2.distance)
 
-    def _skip_if_frozen(self):
-        if self.is_frozen:
-            self.skipTest('Тесты в этом кейсе заморожены')
-
-    def run(self, result=None):
-        self._skip_if_frozen()
-        super().run(result)
-
 
 class TournamentTest(unittest.TestCase):
-    is_frozen = True
+    is_frozen = True  # Атрибут для TournamentTest
     all_results = {}
 
     @classmethod
@@ -53,26 +59,21 @@ class TournamentTest(unittest.TestCase):
         for key, value in cls.all_results.items():
             print(f"{key}: {value}")
 
-    def _skip_if_frozen(self):
-        if self.is_frozen:
-            self.skipTest('Тесты в этом кейсе заморожены')
-
-    def run(self, result=None):
-        self._skip_if_frozen()
-        super().run(result)
-
+    @skip_if_frozen
     def test_race_usain_nick(self):
         tournament = Tournament(90, self.runner1, self.runner3)
         results = tournament.start()
         self.all_results['Усэйн и Ник'] = results
         self.assertTrue(results[len(results)] == "Ник")
 
+    @skip_if_frozen
     def test_race_andrey_nick(self):
         tournament = Tournament(90, self.runner2, self.runner3)
         results = tournament.start()
         self.all_results['Андрей и Ник'] = results
         self.assertTrue(results[len(results)] == "Ник")
 
+    @skip_if_frozen
     def test_race_usain_andrey_nick(self):
         tournament = Tournament(90, self.runner1, self.runner2, self.runner3)
         results = tournament.start()
