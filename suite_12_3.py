9a6aab041a42385308aa9c872a3c5d0a4ab69bc8
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-08-08 18:02:35 +0300

    suite_12_3.py

--- suite_12_3.py
+++ suite_12_3.py
@@ -1,18 +1,14 @@
 import unittest
-from runner import Runner
-from runner_tournament import Tournament
-from your_test_module import RunnerTest, TournamentTest  # Замените your_test_module на название файла с вашими тестами
+from test_runner import RunnerTest
+from test_runner_tournament import TournamentTest
 
-# Создаем объект TestSuite
+
 test_suite = unittest.TestSuite()
 
-# Добавляем тесты RunnerTest и TournamentTest в TestSuite
 test_suite.addTest(unittest.makeSuite(RunnerTest))
 test_suite.addTest(unittest.makeSuite(TournamentTest))
 
-# Создаем объект класса TextTestRunner с verbosity=2
 runner = unittest.TextTestRunner(verbosity=2)
 
-# Запускаем тесты
 if __name__ == '__main__':
     runner.run(test_suite)
