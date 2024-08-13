f63d0cb051b82701645ff0a3e4efbb216c9749ca
lubyantsev <162503570+lubyantsev@users.noreply.github.com>
2024-08-13 06:16:25 +0300

    tests_12_4.py

--- tests_12_4.py
+++ tests_12_4.py
@@ -6,8 +6,39 @@ logging.basicConfig(
     filename='runner_tests.log',
     level=logging.INFO,
     encoding='utf-8',
-    format='%(asctime)s | %(levelname)s | %(message)s'
+    format='%(levelname)s: %(message)s'  # Формат вывода с уровнем логирования и сообщением
 )
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
@@ -27,6 +58,7 @@ class RunnerTest(unittest.TestCase):
         except TypeError as e:
             logging.warning("Неверный тип данных для объекта Runner")
             logging.exception(e)
+
 
 if __name__ == '__main__':
     unittest.main()
